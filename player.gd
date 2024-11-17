eb32e1bddb3c0a34e7714938eefae2d31b4309a4
goshag45 <goshagugl44@gmail.com>
2024-11-17 15:33:25 +1100

    jumping and squashing monsters

--- player.gd
+++ player.gd
@@ -5,6 +5,9 @@ extends CharacterBody3D
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
+@export var jump_imuplse = 20
+@export var bounce_impulse = 16
+
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
@@ -30,6 +33,31 @@ func _physics_process(delta):
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		target_velocity.y += jump_imuplse
+
+	# Iterate through all collisions that occurred this frame
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+
+		# If the collision is with ground
+		if collision.get_collider() == null:
+			#print("no collision")
+			continue
+
+		# If the collider is with a mob
+		if collision.get_collider().is_in_group("mob"):
+			print("collision")
+			var mob = collision.get_collider()
+			# we check that we are hitting it from above.
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				# If so, we squash it and bounce.
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				# Prevent further duplicate calls.
+				break
 
 	# Moving the Character
 	velocity = target_velocity
